ddaa0ccfc9e21fc6f021dc1bfa8721bb668a04c8
hoa nguyen thai <nguyenthaihoamku@gmail.com>
2022-06-02 01:00:21 +0700

    push

---
+++ src/users/user.entity.ts
@@ -0,0 +1,15 @@
+import {Entity, Column, PrimaryGeneratedColumn} from "typeorm";
+import {ApiProperty} from "@nestjs/swagger";
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @ApiProperty()
+    @Column()
+    firstName: string;
+
+    @ApiProperty()
+    @Column()
+    lashName: string;
+}
